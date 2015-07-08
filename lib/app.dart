1ea17f9cf47dd07c70e21c00608be21abad9e168
Andres Araujo <a.araujo.azua@gmail.com>
2015-07-08 11:26:39 -0500

    update to alpha 30

--- lib/app.dart
+++ lib/app.dart
@@ -4,7 +4,7 @@ import 'package:angular2/angular2.dart';
     selector: "my-app"
 )
 @View(
-    templateUrl: "packages/ng2_dart_quickstart/app.html"
+    templateUrl: "app.html"
 )
 class AppComponent {
   String name = "Alice";
