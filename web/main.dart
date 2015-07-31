f7d330a97cf370cf2eb40130586a221d29a5e2c6
Andres Araujo <a.araujo.azua@gmail.com>
2015-07-31 13:21:29 -0500

    chore: update to alpha 33

--- web/main.dart
+++ web/main.dart
@@ -1,5 +1,4 @@
-import 'package:angular2/angular2.dart';
-import 'package:angular2/di.dart';
+import 'package:angular2/bootstrap.dart';
 
 // These imports will go away soon:
 import 'package:angular2/src/reflection/reflection.dart' show reflector;
@@ -12,28 +11,5 @@ void main() {
   // Temporarily needed.
   reflector.reflectionCapabilities = new ReflectionCapabilities();
 
-  bootstrap(AppComponent, [ bind(UrlResolver).toClass(DartUrlResolver) ]);
-}
-
-
-//This is temporary, to enable package: URLs
-// see: https://github.com/angular/angular/issues/2945#issuecomment-119761570
-@Injectable()
-class DartUrlResolver implements UrlResolver {
-  static final _baseUrlResolver = new UrlResolver();
-
-  final String urlPrefix;
-
-  const DartUrlResolver() : urlPrefix = '';
-
-  const DartUrlResolver.withUrlPrefix(this.urlPrefix);
-
-  @override
-  String resolve(String baseUrl, String url) {
-    // Delegate to Angular to get a final URL.
-    final angularResolvedUrl = _baseUrlResolver.resolve(baseUrl, url);
-
-    // If this is a 'package:' style URL, replace with a pub-resolvable one.
-    return angularResolvedUrl.replaceFirst('package:', '${urlPrefix}packages/');
-  }
+  bootstrap(AppComponent);
 }
