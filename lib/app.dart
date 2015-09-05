7887befb65b27efa10b5c0843796a8e3b5e681de
Andres Araujo <a.araujo.azua@gmail.com>
2015-09-05 10:53:03 -0500

    refactor: upgrade to alpha 36

--- lib/app.dart
+++ lib/app.dart
@@ -1,4 +1,4 @@
-import 'package:angular2/angular2.dart';
+import 'package:angular2/angular2.dart' show Component, View;
 
 @Component(
     selector: "my-app"
--- web/main.dart
+++ web/main.dart
@@ -1,5 +1,5 @@
-import 'package:angular2/bootstrap.dart';
-import 'package:ng2_dart_quickstart/app.dart';
+import 'package:angular2/bootstrap.dart' show bootstrap;
+import 'package:ng2_dart_quickstart/app.dart' show AppComponent;
 
 void main() {
   bootstrap(AppComponent);
