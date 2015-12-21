193ebc931520ca1544f3944ec231da61ee54cd95
Andres Araujo <a.araujo.azua@gmail.com>
2015-12-21 17:13:18 -0600

    refactor: upgrade to beta 0

--- lib/app.dart
+++ lib/app.dart
@@ -1,9 +1,7 @@
 import 'package:angular2/angular2.dart' show Component, View;
 
 @Component(
-    selector: "my-app"
-)
-@View(
+    selector: "my-app",
     templateUrl: "package:ng2_dart_quickstart/app.html"
 )
 class AppComponent {
